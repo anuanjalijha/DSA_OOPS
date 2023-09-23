38b54d35562326c4b146bb45714e3af03087a084
Anjali <anjalijanar@gmail.com>
2023-09-23 21:51:57 +0530

    abstract class in oops

---
+++ AbstractClass.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def move(self):
+        pass
+
+class Human(Animal):
+    def move(self):
+        print('I can walk and run')
+        
+class Snake(Animal):
+    def move(self):
+        print('I can crawl')
+         
+         
+class Dog(Animal):
+    def move(self):
+        print('I can bark')
+
+class Lion(Animal):
+    def move(self):
+        print('I can roar')
+        
+h = Human()
+s = Snake()
+d = Dog()
+l = Lion()
+
+h.move()
+s.move()
+d.move()
+l.move()
+        
+        
+                             
