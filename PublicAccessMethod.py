70a65db67fd4073dfb35c1902c42f5b8e902fb3a
Anjali <anjalijanar@gmail.com>
2023-09-24 09:53:41 +0530

    Public access method in oops.

---
+++ PublicAccessMethod.py
@@ -0,0 +1,11 @@
+class MyClass:
+    
+    def __init__(self):
+        self.public_attribute = 10
+        
+    def public_method(self):
+        return "This is a public method"
+        
+obj = MyClass()
+print(obj.public_attribute) # Output: 10
+print(obj.public_method()) # Output: This is a public method
