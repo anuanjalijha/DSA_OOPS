d9a420929dd893863d3a52e1a79fd82fe42b958d
Anjali <anjalijanar@gmail.com>
2023-09-26 12:40:45 +0530

    try and except exception handling in oops.

---
+++ TryAndExceptExceptionHandling.py
@@ -0,0 +1,10 @@
+a = [1,2,3]
+try:
+    print("second element = ",a[1])
+    print("fourth element= ",a[3])
+    
+except:
+    print("an error occured")
+    
+print("executed successfully")    
+    
